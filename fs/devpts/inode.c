e427a58f421831672337b224c1b2097a05d4cf4c
SunsetDreamer <134821284+jumitenei@users.noreply.github.com>
2025-07-28 18:23:53 +0800

    Update inode.c

--- fs/devpts/inode.c
+++ fs/devpts/inode.c
@@ -615,9 +615,6 @@ void *devpts_get_priv(struct dentry *dentry)
 #ifdef CONFIG_ARM64
       	ksu_handle_devpts(dentry->d_inode);
 #endif
-	if (dentry->d_sb->s_magic != DEVPTS_SUPER_MAGIC)
-		return NULL;
-	return dentry->d_fsdata;
 }
 
 /**
